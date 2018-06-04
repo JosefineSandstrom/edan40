9f3b26e33d0fef00009f35d7a9d9a8193dd8069f
Andreas Sirenius <a.sirenius@gmail.com>
2018-06-04 11:19:07 +0200

    tried to fix things

--- assignment1/Chatterbot.hs
+++ assignment1/Chatterbot.hs
@@ -2,7 +2,7 @@ module Chatterbot where
   import Utilities
   import System.Random
   import Data.Char
-  
+
   chatterbot :: String -> [(String, [String])] -> IO ()
   chatterbot botName botRules = do
       putStrLn ("\n\nHi! I am " ++ botName ++ ". How are you?")
@@ -15,36 +15,31 @@ module Chatterbot where
         answer <- stateOfMind brain
         putStrLn (botName ++ ": " ++ (present . answer . prepare) question)
         if (not . endOfDialog) question then botloop else return ()
-  
+
   --------------------------------------------------------
-  
+
   type Phrase = [String]
   type PhrasePair = (Phrase, Phrase)
   type BotBrain = [(Phrase, [Phrase])]
-  
+
   --------------------------------------------------------
-  
+
   stateOfMind :: BotBrain -> IO (Phrase -> Phrase)
-  stateOfMind bb = do
+  stateOfMind bb = do --ok
     r <- randomIO :: IO Float
     let tmp1 = fst $ unzip bb
         tmp2 = snd $ unzip bb
         tmp3 = map (pick r) tmp2
-        pattern = zip tmp1 tmp3
-    return (rulesApply pattern)
+        patterns = zip tmp1 tmp3
+    return (rulesApply patterns)
   
+  -- fixed
   rulesApply :: [PhrasePair] -> Phrase -> Phrase
-  rulesApply pattern phrase
-        | tmp == Nothing = words ""
-        | otherwise      = words (try f (unwords phrase))
-        where tmp = f (unwords phrase)
-              f x = transformationsApply '*' (unwords . reflect . words) stringPattern' (map toLower x)
-              stringPattern = phrasePairToStringPair pattern
-              stringPattern' = map (\x -> map2 (map toLower, id) (fst x, snd x)) stringPattern
-          
+  rulesApply = (maybe [] id .) . transformationsApply "*" reflect
+
   reflect :: Phrase -> Phrase
-  reflect = (map.try) (flip lookup reflections)
-  
+  reflect = (map.try) (flip lookup reflections) --nice
+
   reflections =
     [ ("am",     "are"),
       ("was",    "were"),
@@ -63,23 +58,23 @@ module Chatterbot where
       ("yours",  "mine"),
       ("you",    "me")
     ]
-  
+
   ---------------------------------------------------------------------------------
-  
+
   endOfDialog :: String -> Bool
   endOfDialog = (=="quit") . map toLower
-  
+
   present :: Phrase -> String
   present = unwords
-  
+
   prepare :: String -> Phrase
   prepare = reduce . words . map toLower . filter (not . flip elem ".,:;*!#%&|")
-  
+
   rulesCompile :: [(String, [String])] -> BotBrain
-  rulesCompile = (map . map2) (words, map words)
-  
+  rulesCompile = (map . map2) (words, map words) --nice
+
   --------------------------------------
-  
+
   reductions :: [PhrasePair]
   reductions = (map.map2) (words, words)
     [ ( "please *", "*" ),
@@ -94,67 +89,65 @@ module Chatterbot where
       ( "i am very *", "i am *" ),
       ( "hi *", "hello *")
     ]
-  
+
   reduce :: Phrase -> Phrase
   reduce = fix $ reductionsApply reductions
   
+  -- fixed
   reductionsApply :: [PhrasePair] -> Phrase -> Phrase
-  reductionsApply reductions =  words . try (transformationsApply '*' id $ phrasePairToStringPair reductions) . unwords
-  
-  phrasePairToStringPair :: [([String], [String])] -> [(String, String)]
-  phrasePairToStringPair = (map . map2) (unwords, unwords)
-  
-  stringPairToPhrasePair :: [(String, String)] -> [([String], [String])]
-  stringPairToPhrasePair = (map . map2) (words, words)
-  
+  reductionsApply reductions =  fix $ try (transformationsApply "*" id reductions)
+
   -------------------------------------------------------
   -- Match and substitute
   --------------------------------------------------------
-  
+
+  -- fixed
   substitute :: Eq a => a -> [a] -> [a] -> [a]
   substitute _ [] _ = []
-  substitute wc (t:ts) s = (if t == wc then s else [t]) ++ (substitute wc ts s)
+  substitute wc (t:ts) s
+    | t == wc   = s ++ substitute wc ts s
+    | otherwise = [t] ++ substitute wc ts s
   
+  -- fixed
   match :: Eq a => a -> [a] -> [a] -> Maybe [a]
   match _ [] [] = Just []
   match _ [] _ = Nothing
   match _ _ [] = Nothing
   match wc (p:ps) (s:ss)
-      | p /= wc   = if p == s then match wc ps ss else Nothing
-      | otherwise = single `orElse` longer
+      | p == wc   = orElse single longer
+      | p == s    = match wc ps ss
+      | otherwise = Nothing
       where single = singleWildcardMatch (p:ps) (s:ss)
             longer = longerWildcardMatch (p:ps) (s:ss)
   
+  -- fixed          
   singleWildcardMatch, longerWildcardMatch :: Eq a => [a] -> [a] -> Maybe [a]
-  singleWildcardMatch (wc:ps) (x:xs) = if match wc ps xs == Nothing then Nothing else Just [x]
-  
-  longerWildcardMatch (wc:ps) (x:xs)
-      | tmp == Nothing  = Nothing
-      | otherwise       = mmap ([x] ++) tmp
-      where tmp = match wc (wc:ps) xs
-  
+  singleWildcardMatch (wc:ps) (x:xs) = mmap (const [x]) (match wc ps xs)
+  -- fixed      
+  longerWildcardMatch (wc:ps) (x:xs) = mmap (x:) (match wc (wc:ps) xs)
+
   -- Test cases --------------------
-  
+
   testPattern =  "a=*;"
   testSubstitutions = "32"
   testString = "a=32;"
-  
+
   substituteTest = substitute '*' testPattern testSubstitutions
   substituteCheck = substituteTest == testString
-  
+
   matchTest = match '*' testPattern testString
   matchCheck = matchTest == Just testSubstitutions
-  
+
   -------------------------------------------------------
   -- Applying patterns
   --------------------------------------------------------
-  
+
   transformationApply :: Eq a => a -> ([a] -> [a]) -> [a] -> ([a], [a]) -> Maybe [a]
-  transformationApply wc f s pt = mmap (substitute wc (snd pt)) (mmap f (match wc (fst pt) s))
-  
+  transformationApply wc f s pt = mmap (substitute wc (snd pt)) (mmap f (match wc (fst pt) s)) --nice
+
   transformationsApply :: Eq a => a -> ([a] -> [a]) -> [([a], [a])] -> [a] -> Maybe [a]
   transformationsApply _ _ [] _ = Nothing
-  transformationsApply wc f (pt:pts) s
+  transformationsApply wc f (pt:pts) s --ok
     | tmp == Nothing   = transformationsApply wc f pts s
     | otherwise        = tmp
     where tmp = transformationApply wc f s pt
